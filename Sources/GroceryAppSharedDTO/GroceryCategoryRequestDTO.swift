3dfa62916aaf513c1d3698e49890b1a47947d2ce
toughtosay <113202638+toughtosay@users.noreply.github.com>
2024-01-19 12:45:16 +0100

    *

--- Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
